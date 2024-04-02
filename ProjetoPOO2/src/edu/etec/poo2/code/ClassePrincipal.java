868c729476c4ec40158f39392ae8254163a3985f
lab04 <lab04@lab4-03>
2024-04-01 21:57:09 -0300

    Adicionado codigo.

--- ProjetoPOO2/src/edu/etec/poo2/code/ClassePrincipal.java
+++ ProjetoPOO2/src/edu/etec/poo2/code/ClassePrincipal.java
@@ -3,7 +3,8 @@ package edu.etec.poo2.code;
 public class ClassePrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello, world!");
 
 	}
 
